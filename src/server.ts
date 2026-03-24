819abcd8f5be5710c9787eefc900b8a1a667f27a
SOHUM SUKHATANKAR <ssukhatankar@college.harvard.edu>
2026-03-24 15:55:35 -0400

    nice looking

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createServer } from 'http';
+import path from 'path';
 import { config } from './config';
 import { createSocketServer } from './realtime/SocketServer';
 import { registerLobbyHandlers } from './realtime/LobbyNamespace';
@@ -7,6 +8,7 @@ import { registerTableHandlers } from './realtime/TableNamespace';
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/health', (_req, res) => res.json({ status: 'ok' }));
 
